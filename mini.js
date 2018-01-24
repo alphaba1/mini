c7d5e219bc713e68bc384ea4a9b35a2e56a3cc8e
alpha ba <baphal1988@hotmail.fr>
2018-01-24 09:56:35 -0500

    finalexercice

---
+++ mini.js
@@ -0,0 +1,73 @@
+function Multiplier{
+	this.currentValue = 1;
+	this.getCurrentValue = function() {
+		return this.currentValue;
+	};
+	this.multiply = {
+		this.currentvalue = this.currentValue *myDigit;
+// 	}
+getCurrentValue(){
+	return this.currentvalue* mydigit;
+}
+
+}
+ 
+ var multiply = new ();
+
+ console.log(Test.getCurrentValue());
+ console.log(Test.multiply(3));
+ console.log(Test.getCurrentValue());
+ console.log(Test.multiply(7));
+ console.log(Test.getCurrentValue());
+
+
+ function Photos(,){
+ 	this.happyCats = happyCats;
+ 	this.hungryCats = hungryCats;
+ 	this.scaryCats = scaryCats;
+
+ }
+
+function Person(major, gender) {
+	this.gender = gender;
+	this.major = major;
+}
+
+function Teacher(){
+	person.apply(this, arguments)
+
+}
+function Student(){
+	person.apply(this,arguments)
+
+}
+var teacher = new Person('teaching','male');
+var student = new Person('learning','female');
+
+
+ 
+Person.prototype.school = function(){
+	return this.teacher + this.student
+	}
+
+class Album{
+	constructor(photos){
+		this.photo = [catPic];
+	}
+	addnewpic(input){
+		this.photo.push(input)
+	}
+	listphotos(){
+		for (var i = 0; i < this.photo.length; i++){
+		return this.Photo[i]
+	}
+	accessphoto(length)
+	 return this.Photo[length]
+	 
+class Photo(color,location){
+	this.color = color;
+    this.location = location;
+	}
+
+var pic1 = new Photo(blue, item1)
+var pic2 = new Photo
